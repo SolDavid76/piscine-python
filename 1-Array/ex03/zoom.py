7e5fccd00a41ca8b5bb645b0145fa9609ef4bf2e
SolDavid76 <david.janusz75@gmail.com>
2024-11-29 21:23:24 +0100

    need to go at school to test

--- 1-Array/ex03/zoom.py
+++ 1-Array/ex03/zoom.py
@@ -1,15 +1,19 @@
 from load_image import ft_load
+import matplotlib.pyplot as plt
 import os
 
 
 def main():
     try:
+        print("A")
         if not os.path.exists("../animal.jpeg"):
             raise AssertionError("File not found")
         img = ft_load("../animal.jpeg")
         if img is None:
             raise AssertionError("Failed to load image")
-        print
+        plt.imshow(img)
+        plt.axis("off")
+        plt.show()
     except AssertionError as error:
         print("AssertionError:", error)
 
