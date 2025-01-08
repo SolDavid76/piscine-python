da94dd9c72c7796f221fec40b37d75771cf2d822
SolDavid76 <david.janusz75@gmail.com>
2025-01-08 16:40:40 +0100

    ex01

--- 2-DataTable/ex01/aff_life.py
+++ 2-DataTable/ex01/aff_life.py
@@ -1,9 +1,20 @@
 from load_csv import load
+import matplotlib.pyplot as plt
 
 
 def main():
-    test = load("../life_expectancy_years.csv")
-    print(test[test["country"] == "France"])
+    data = load("../life_expectancy_years.csv")
+    france_data = data[data["country"] == "France"]
+    years = france_data.columns[1:]
+    life_expetancy = france_data.values[0][1:]
+    plt.plot(years, life_expetancy, label="France")
+    plt.title("France Life expectancy Projections")
+    plt.xlabel("Year")
+    plt.xticks(years[::40], rotation=45)
+    plt.ylabel("Life expectancy")
+    plt.yticks(range(30, 100, 10))
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
